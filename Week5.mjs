f012801e41964b40cf5ff5106facacfcde659f2a
Brichos <nicholasak@uia.no>
2026-02-01 17:39:27 +0100

    Made String Formatter function

--- Week5.mjs
+++ Week5.mjs
@@ -1,4 +1,15 @@
-//Task 1
+//Calculator
 export function multiply(a,b){
     return a*b;
+}
+
+//String Formatter
+export function format_name(first, last) {
+  const cleanFirst = first.trim().toLowerCase();
+  const cleanLast = last.trim().toLowerCase();
+  
+  const capitalizeFirst = cleanFirst.charAt(0).toUpperCase() + cleanFirst.slice(1);
+  const capitalizeLast = cleanLast.charAt(0).toUpperCase() + cleanLast.slice(1);
+  
+  return `${capitalizeLast}, ${capitalizeFirst}`;
 }
