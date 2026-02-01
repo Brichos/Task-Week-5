24367837b65d9922da19d20a7464f32932e9808d
Brichos <nicholasak@uia.no>
2026-02-01 20:03:37 +0100

    Added empty array test

--- Week5Test.mjs
+++ Week5Test.mjs
@@ -67,4 +67,8 @@ describe('getEvenNumbers()', () => {
     test('filters [1,2,3,4,5,6] to [2,4,6]', () => {
         assert.deepEqual(getEvenNumbers([1, 2, 3, 4, 5, 6]), [2, 4, 6]);
         });
+
+    test('handles empty array', () => {
+        assert.deepEqual(getEvenNumbers([]), []);
+        });
 });
