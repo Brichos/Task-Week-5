cf736c77d7ee380d9a4a8bac6f6050a15b8e2e55
Brichos <nicholasak@uia.no>
2026-02-01 17:59:07 +0100

    Added test with positive numbers

--- Week5Test.mjs
+++ Week5Test.mjs
@@ -1,1 +1,12 @@
 import { multiply } from './Week5.mjs';
+
+//Calculator tests
+ const multiplyTester = test("Testing multiply function")
+
+ //Testing with positive numbers
+multiplyTester.isEqual(multiply(2,3),6, "2 * 3 is equal to 6");
+multiplyTester.isEqual(multiply(6,7),32, "6 * 7 is equal to 32"); 
+
+ //Testing with zero
+
+ //Testing with negative numbers
